725119db5852d93dcf0eef8cb9d71b3ac2e16576
Simon Katz <git@nomistech.com>
2013-05-13 13:20:48 +0100

    Add print statements to show when namespaces are loaded.

--- src/midje_protocols_records_in_different_ns/protocol.clj
+++ src/midje_protocols_records_in_different_ns/protocol.clj
@@ -1,4 +1,6 @@
 (ns midje-protocols-records-in-different-ns.protocol)
 
+(println "Loading `defprotocol` namespace")
+
 (defprotocol P
   (f [self]))
--- src/midje_protocols_records_in_different_ns/record.clj
+++ src/midje_protocols_records_in_different_ns/record.clj
@@ -2,6 +2,8 @@
   (require [midje-protocols-records-in-different-ns.protocol
             :refer [P]]))
 
+(println "Loading `defrecord` namespace")
+
 (defrecord R
     []
   P
